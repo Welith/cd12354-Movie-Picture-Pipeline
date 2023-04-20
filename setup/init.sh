85717af3a12b4aae930f5089a67b50eb3e24df7e
Boris Kolev <bkolev95@gmail.com>
2023-04-20 21:08:20 +0800

    * Testing the new build

--- setup/init.sh
+++ setup/init.sh
@@ -6,10 +6,10 @@ userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
 
 # Download tool for manipulating aws-auth
 echo "Downloading tool..."
-curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
+curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.5.9/aws-iam-authenticator_0.5.9_darwin_amd64
 chmod +x aws-iam-authenticator
 
-aws eks update-kubeconfig --name cluster --region us-east-1
+#aws eks update-kubeconfig --name cluster --region us-east-1
 
 echo "Updating permissions"
 ./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
